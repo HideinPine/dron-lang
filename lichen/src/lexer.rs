f1869da86f43a32c7a8469c9326cb96c3d306792
Alvan <alvanngeno@gmail.com>
2026-09-10 19:44:30 +0300

    some

--- lichen/src/lexer.rs
+++ lichen/src/lexer.rs
@@ -1,8 +1,8 @@
+#![allow(unused)]
 pub mod types{
-  #![allow(unused)]
   pub struct LexerCartegories {
     keyword: TokenLiteral,
-    separator: TokenSeparator,
+    separator: TokenSeparator, /* First to do coz it's simpler */
     literal: TokenLiteral,
     operator: TokenOperator,
   }
@@ -11,12 +11,13 @@ pub mod types{
     Enum,
     Struct,
   }
-  enum TokenSeparator {
+  pub enum TokenSeparator {
     OpenParent,
     CloseParent,
     SemiColon,
     LeftCurl,
     RightCurl,
+    Unknown,
   }
   enum TokenLiteral {
     True,
@@ -30,6 +31,8 @@ pub mod types{
   }
 }
 pub mod checker {
+  use crate::lexer::types::LexerCartegories;
+
   /* Determine the next plausible value and then match to the one needed */
   fn get_nxt_value(chars: Vec<char>, i: usize)  {
     let char_value = chars[i];
@@ -41,14 +44,16 @@ pub mod checker {
   pub fn check_values(chars: Vec<char>) {
     for (i, char) in chars.iter().enumerate() {
       if char.is_numeric() {
-        get_nxt_value(chars.clone(), i);
+        //get_nxt_value(chars.clone(), i);
         println!("Digit character: {}, {}", char, i);
       } else if char.is_whitespace() {
         println!("Whitespace character: {}", char);
       } else if char.is_alphabetic() {
         println!("Alphabetic character: {}", char);
-      } else{
-        println!("Other character: {}", char);
+      } else if char.is_ascii_punctuation(){
+        match char {
+          _ => println!("Match stuff not done for char: {}",char),
+        }
       }
     }
   }
