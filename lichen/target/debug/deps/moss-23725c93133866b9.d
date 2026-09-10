5a4645ddac5c0806e6d95bf51cae4c3b1a64d6b3
Alvan <alvanngeno@gmail.com>
2026-09-10 09:05:53 +0300

    target rm

--- lichen/target/debug/deps/moss-23725c93133866b9.d
+++
@@ -1,5 +0,0 @@
-C:\Users\ADMIN\Projects\Moss\moss\target\debug\deps\moss-23725c93133866b9.d: src\main.rs
-
-C:\Users\ADMIN\Projects\Moss\moss\target\debug\deps\moss-23725c93133866b9.exe: src\main.rs
-
-src\main.rs:
--- lichen/target/debug/moss.d
+++
@@ -1,1 +0,0 @@
-C:\Users\ADMIN\Projects\Moss\moss\target\debug\moss.exe: C:\Users\ADMIN\Projects\Moss\moss\src\main.rs
